0975db368e4526ce8a02f467d9f24a324e4a6130
AReye85 <areye85@illinois.edu>
2026-09-23 01:25:43 -0500

    fa guard works now
    im dakmstmr

---
+++ Assets/Scripts/Guard_Behavior.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Guard_Behavior : MonoBehaviour
+{
+    public Vector3 end;
+    private float duration = 2f;
+    public GameObject money;
+    private bool moving = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving) {
+            transform.position = Vector3.Lerp(transform.position, end, duration * Time.deltaTime);
+            if (transform.position == end) {
+                moving = false;
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject == money) {
+            Debug.Log("MONEY!");
+            moving = true;
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.CompareTag("Player")) {
+            Debug.Log("No.");
+        }
+    }   
+}
